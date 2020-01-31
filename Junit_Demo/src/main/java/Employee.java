7f1628597f530d497c73721691b3c4911a9aa7d4
shivanidahiwal <60462000+shivanidahiwal@users.noreply.github.com>
2020-01-31 15:47:40 +0530

    Add files via upload

---
+++ Junit_Demo/src/main/java/Employee.java
@@ -0,0 +1,22 @@
+
+public class Employee {
+
+	public Employee() {
+
+	}
+	
+	private int Id;
+	private String name;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ Junit_Demo/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee emp = new Employee();
+		emp.setId(101);
+		emp.setName("Shivani");
+		System.out.println("name = " + emp.getName() +"ID = " + emp.getId());
+	}
+
+}
